ce5e733c1fb7dc87f6b1c75df27ea54ad22e8fb6
Khofidin <offiedz@gmail.com>
2019-10-11 20:34:50 +0700

    Setup assets and project

--- app/src/main/java/in/khofid/academy/ui/home/HomeActivity.java
+++ app/src/main/java/in/khofid/academy/ui/home/HomeActivity.java
@@ -1,32 +1,62 @@
 package in.khofid.academy.ui.home;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 import in.khofid.academy.R;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            switch (menuItem.getItemId()) {
+                case R.id.action_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.action_bookmark:
+                    mTextMessage.setText(R.string.title_dashboard);
+                    return true;
+            }
+            return false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        mTextMessage = findViewById(R.id.message);
+
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
+
+        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        /*
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+
+         */
     }
 
 }
