ffc22509556e5571dce5cc0330f4cca585703049
Khofidin <offiedz@gmail.com>
2019-10-19 09:12:09 +0700

    Testing ViewModel and UI

---
+++ app/src/androidTest/java/in/khofid/academy/ui/academy/AcademyFragmentTest.java
@@ -0,0 +1,35 @@
+package in.khofid.academy.ui.academy;
+
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import in.khofid.academy.R;
+import in.khofid.academy.testing.SingleFragmentActivity;
+import in.khofid.academy.utils.RecyclerViewItemCountAssertion;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+public class AcademyFragmentTest {
+
+    @Rule
+    public ActivityTestRule<SingleFragmentActivity> activityRule = new ActivityTestRule<>(SingleFragmentActivity.class);
+    private AcademyFragment academyFragment = new AcademyFragment();
+
+    @Before
+    public void setUp(){
+        activityRule.getActivity().setFragment(academyFragment);
+    }
+
+    @Test
+    public void loadCourses() {
+        onView(withId(R.id.rv_academy)).check(matches(isDisplayed()));
+        onView(withId(R.id.rv_academy)).check(new RecyclerViewItemCountAssertion(5));
+    }
+
+}
---
+++ app/src/androidTest/java/in/khofid/academy/ui/bookmark/BookmarkFragmentTest.java
@@ -0,0 +1,34 @@
+package in.khofid.academy.ui.bookmark;
+
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import in.khofid.academy.R;
+import in.khofid.academy.testing.SingleFragmentActivity;
+import in.khofid.academy.utils.RecyclerViewItemCountAssertion;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+public class BookmarkFragmentTest {
+
+    @Rule
+    public ActivityTestRule<SingleFragmentActivity> activityTestRule = new ActivityTestRule<>(SingleFragmentActivity.class);
+    private BookmarkFragment bookmarkFragment = new BookmarkFragment();
+
+    @Before
+    public void setUp(){
+        activityTestRule.getActivity().setFragment(bookmarkFragment);
+    }
+
+    @Test
+    public void loadBookmarks(){
+        onView(withId(R.id.rv_bookmark)).check(matches(isDisplayed()));
+        onView(withId(R.id.rv_bookmark)).check(new RecyclerViewItemCountAssertion(5));
+    }
+}
